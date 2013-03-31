427e71d7d5f97274cb92dd04442ccb6afc48819a
MozMorris <moz@earthview.co.uk>
2013-03-31 16:04:45 +0100

    Improve coding stadards

--- javascripts/slideshow.js
+++ javascripts/slideshow.js
@@ -1,132 +1,131 @@
 // Slideshow Module
-var Slideshow = (function () {
+var slideshow = (function () {
   "use strict";
 
   // slideshow elements
-  var actvSlide = $('#actv-slide'),
-  next_slide = $('#next-slide'),
-  next_btn = $('#next-btn'),
-  previous_slide = $('#prev-slide'),
-  prev_btn = $('#prev-btn'),
-
+  var actvSlide = $('#actv-slide');
+  var nextSlide = $('#next-slide');
+  var nextBtn = $('#next-btn');
+  var prevSlide = $('#prev-slide');
+  var prevBtn = $('#prev-btn');
 
   // disables next / prev buttons while content is loading
-  disableNav = function() {
+  var disableNav = function() {
     $('#controls a').on('click', function() { return false; });
     $('#controls a').off('click', navigate);
     $(document).off('keyup', navigate);
     $(window).off('popstate', updateState);
-  },
+  };
 
   // enables navigation
-  enableNav = function() {
+  var enableNav = function() {
     $('#controls a').on('click', navigate);
     $(document).on('keyup', navigate);
     $(window).on('popstate', updateState);
-  },
+  };
 
   // preloads previous / next slides (determined by next/previous buttons)
-  loadPreviousNext = function() {
-    previous_slide.load($('.prev-next-lnks .prev', actvSlide).attr('href') + ' #actv-slide .content', function() {
-      next_slide.load($('.prev-next-lnks .next', actvSlide).attr('href') + ' #actv-slide .content', enableNav);
+  var loadPreviousNext = function() {
+    prevSlide.load($('.prev-next-lnks .prev', actvSlide).attr('href') + ' #actv-slide .content', function() {
+      nextSlide.load($('.prev-next-lnks .next', actvSlide).attr('href') + ' #actv-slide .content', enableNav);
     });
-  },
+  };
 
   // detects which button has been clicked
-  navigate = function(e) {
+  var navigate = function(e) {
     e.preventDefault();
     disableNav();
 
     var state = {};
 
-    if (this == prev_btn[0]) {
+    if (this == prevBtn[0]) {
       previous();
       state = {
         direction: 'prev',
-        url: prev_btn.attr('href')
+        url: prevBtn.attr('href')
       };
-    } else if (this == next_btn[0] || e.which == 32) {
+    } else if (this == nextBtn[0] || e.which == 32) {
       next();
       state = {
         direction: 'next',
-        url: next_btn.attr('href')
+        url: nextBtn.attr('href')
       };
     }
 
     if (typeof state.url != 'undefined') {
       window.history.pushState({ actvSlide: state.url }, document.title, state.url);
     }
-  },
+  };
 
   // moves to the next slide
-  next = function() {
+  var next = function() {
     actvSlide.animate({
       left: '-20%'
     }, 500, 'swing', function() {
-      next_slide.animate({
+      nextSlide.animate({
         left: '0%'
       }, 500, 'swing', updateSlide);
     });
-  },
+  };
 
   // moves to the previous slide
-  previous = function() {
+  var previous = function() {
     actvSlide.animate({
       left: '60%'
     }, 500, 'swing', function() {
-      previous_slide.animate({
+      prevSlide.animate({
         left: '40%'
       }, 500, 'swing', updateSlide);
     });
-  },
+  };
 
   // updates links for next / prev buttons
-  updateLinks = function() {
+  var updateLinks = function() {
     var prevLink = $('.prev-next-lnks .prev', actvSlide),
     nextLink = $('.prev-next-lnks .next', actvSlide);
 
     if (prevLink[0]) {
-      prev_btn.attr('href', prevLink.attr('href'));
+      prevBtn.attr('href', prevLink.attr('href'));
     }
 
     if (nextLink[0]) {
-      next_btn.attr('href', nextLink.attr('href'));
+      nextBtn.attr('href', nextLink.attr('href'));
     }
-  },
+  };
 
   // updates active slide
-  updateSlide = function() {
-    if (this == next_slide[0]) {
-      previous_slide.remove();
-      previous_slide = actvSlide;
-      previous_slide.attr('id', 'prev-slide');
-      actvSlide = next_slide;
+  var updateSlide = function() {
+    if (this == nextSlide[0]) {
+      prevSlide.remove();
+      prevSlide = actvSlide;
+      prevSlide.attr('id', 'prev-slide');
+      actvSlide = nextSlide;
       actvSlide.attr('id', 'actv-slide');
       updateLinks();
-      next_slide = $('<div class="slide" id="next-slide"></div>').appendTo('#slides-wrapper');
-      next_slide.load($('.prev-next-lnks .next', actvSlide).attr('href') + ' #actv-slide .content', postLoad);
+      nextSlide = $('<div class="slide" id="next-slide"></div>').appendTo('#slides-wrapper');
+      nextSlide.load($('.prev-next-lnks .next', actvSlide).attr('href') + ' #actv-slide .content', postLoad);
     } else {
-      next_slide.remove();
-      next_slide = actvSlide;
-      next_slide.attr('id', 'next-slide');
-      actvSlide = previous_slide;
+      nextSlide.remove();
+      nextSlide = actvSlide;
+      nextSlide.attr('id', 'next-slide');
+      actvSlide = prevSlide;
       actvSlide.attr('id', 'actv-slide');
       updateLinks();
-      previous_slide = $('<div class="slide" id="prev-slide"></div>').prependTo('#slides-wrapper');
-      previous_slide.load($('.prev-next-lnks .prev', actvSlide).attr('href') + ' #actv-slide .content', postLoad);
+      prevSlide = $('<div class="slide" id="prev-slide"></div>').prependTo('#slides-wrapper');
+      prevSlide.load($('.prev-next-lnks .prev', actvSlide).attr('href') + ' #actv-slide .content', postLoad);
     }
-    $([previous_slide, actvSlide, next_slide]).each(function(i, e) {
+    $([prevSlide, actvSlide, nextSlide]).each(function(i, e) {
       this.css('left', '');
     });
-  },
+  };
 
   // update the current state of the slideshow
-  updateState = function(e) {
+  var updateState = function(e) {
     console.log(e.originalEvent.state);
-  },
+  };
 
   // performs any tasks after the next / previous slides have been loaded
-  postLoad = function() {
+  var postLoad = function() {
     // the buttons work again
     enableNav();
   };
